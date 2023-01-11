73f33d78a11af024db98bb8f5515181589ed1483
Ana Delic <milojevican@gmail.com>
2023-01-11 22:26:39 +0100

    Chane index.js

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
 import axios from 'axios';
+import * as cheerio from 'cheerio';
 
+//main url
 const websiteUrl = 'https://memegen-link-examples-upleveled.netlify.app/';
+
+axios
+  .get(websiteUrl)
+  .then(function (response) {
+    const html = response.data;
+    const $ = cheerio.load(html);
+    const urls = [];
+
+    $('img', html).each(function () {
+      const url = $(this).attr('src');
+      urls.push(url);
+      console.log(urls.slice(1, 2));
+    });
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  });

1323a3721ade32c37cb5c7784d35f788712a18d8
Ana Delic <milojevican@gmail.com>
2023-01-12 12:14:29 +0100

    Make memes folder

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 import axios from 'axios';
 import * as cheerio from 'cheerio';
+import * as fs from 'fs';
 
 //main url
 const websiteUrl = 'https://memegen-link-examples-upleveled.netlify.app/';
 
+//making a folder
+
+try {
+  if (!fs.existsSync('./memes')) {
+    fs.mkdirSync('./memes');
+  }
+  //handle error
+} catch (err) {
+  console.error(err);
+}
+
+//making a request
 axios
   .get(websiteUrl)
   .then(function (response) {
@@ -15,7 +28,8 @@ axios
       const url = $(this).attr('src');
       urls.push(url);
     });
-    console.log(urls.slice(0, 10));
+    const someImgs = urls.slice(0, 10);
+    console.log(someImgs);
   })
   .catch(function (error) {
     // handle error
